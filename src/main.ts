b0a5c6c7e30d3d2d15832cf7fd357f9646b20b5f
Jonathan Larsson <jonte1275@gmail.com>
2023-07-21 08:56:05 +0200

    server works now

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+//import { Cors } from "cors"
+import * as cors from 'cors';
+import { ValidationPipe } from '@nestjs/common'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.useGlobalPipes(new ValidationPipe);
+  
+  await app.listen(3000);
+}
+bootstrap();
