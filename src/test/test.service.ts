0565b96f89376472c47ef492ddb1d92dedf472b7
Jonathan <jonte1275@gmail.com>
2023-07-23 19:30:51 +0200

    Rest API works

--- src/test/test.service.ts
+++ src/test/test.service.ts
@@ -2,29 +2,29 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Test } from './schemas/test.schema';
-import { CreateCatDto } from './dto/create-cat.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class TestService {
-    constructor(@InjectModel(Test.name) private readonly catModel: Model<Test>) {}
+    constructor(@InjectModel(Test.name) private readonly userModel: Model<Test>) {}
 
-  async create(createCatDto: CreateCatDto): Promise<Test> {
-    const createdCat = await this.catModel.create(createCatDto);
-    return createdCat;
+  async create(createUserDto: CreateUserDto): Promise<Test> {
+    const createdUser = await this.userModel.create(createUserDto);
+    return createdUser;
   }
 
   async findAll(): Promise<Test[]> {
-    return this.catModel.find().exec();
+    return this.userModel.find().exec();
   }
 
   async findOne(id: string): Promise<Test> {
-    return this.catModel.findOne({ _id: id }).exec();
+    return this.userModel.findOne({ _id: id }).exec();
   }
 
   async delete(id: string) {
-    const deletedCat = await this.catModel
+    const deletedUser = await this.userModel
       .findByIdAndRemove({ _id: id })
       .exec();
-    return deletedCat;
+    return deletedUser;
   }
 }
