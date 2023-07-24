f3e8cd7263b3995be8669714966c230cd110a2b1
Jonathan Larsson <jonte1275@gmail.com>
2023-07-24 09:05:54 +0200

    oops

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://adm-jonte:4tuQJqFAqVCDRf@atlascluster.qnebpqi.mongodb.net/'), TestModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://adm-jonte:<passwrd>@atlascluster.qnebpqi.mongodb.net/'), TestModule],
 })
 export class AppModule {}
