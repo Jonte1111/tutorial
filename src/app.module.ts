5efefd33630b9b929e5631d474039c86853c4ab4
Jonathan Larsson <jonte1275@gmail.com>
2023-07-24 10:34:35 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TestModule } from './test/test.module';
-
+//test
 @Module({
   imports: [MongooseModule.forRoot('mongodb+srv://adm-jonte:<passwrd>@atlascluster.qnebpqi.mongodb.net/'), TestModule],
 })
